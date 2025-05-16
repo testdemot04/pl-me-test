498ee9b6db08b759c9edc8ee492edb5a8146ab7f
Mahendra J <no-reply@example.com>
2025-05-16 19:30:25 +0530

    Update login.test.ts

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -4,7 +4,7 @@ import { HomePage } from '../pages/HomePage';
 
 test("login should be successfull",async({pages}) =>{
     await pages.loginPage.navigate_to_url();
-    await pages.loginPage.Login_as_admin();
+    //await pages.loginPage.Login_as_admin();
     await pages.homePage.verify_homePage_title();
     // const icon=page.locator("//ul[@id='side-menu']/li");
     // const count= await icon.count();
